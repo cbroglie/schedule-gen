597fcd5d369ea027e1c4d1e4b60058961d4efd30
Chris Broglie <cbroglie@zynga.com>
2012-09-03 16:47:45 -0700

    Add constraint for max number of home games (no more than half), randomize matchups, and add memoization cache.

--- libsched/schedule.py
+++ libsched/schedule.py
@@ -1,9 +1,38 @@
 import copy
+from functools import wraps
+import pdb
+import random
+
+def memoize(f):
+    fname = f.__name__
+    disable = False
+
+    @wraps(f)
+    def memoizedFunction(self, team, *args):
+        #print "in memoize %r" % fname,
+        #if disable: 
+        #    print "...dirty"
+        #    return f(self, team, *args)
+        #pdb.set_trace()
+        if not team in self.cache:
+            self.cache[team] = {}
+        if not fname in self.cache[team]:
+            self.cache[team][fname] = False
+        if not self.cache[team][fname]:
+            self.cache[team][fname] = {}
+        if not args in self.cache[team][fname]:     
+            self.cache[team][fname][args] = f(self, team, *args)
+        #    print "...dirty"
+        #else:
+        #    print "...cached"
+        return self.cache[team][fname][args]
+    return memoizedFunction
 
 class Schedule:
     def __init__(self):
         self.num_weeks = 1
         self.num_teams = 2
+        self.cache = {}
 
     def init(self):
         """Setup the initial state based on the number of teams and weeks"""
@@ -27,8 +56,13 @@ class Schedule:
 
         # Generate the initial schedule which is a dictionary of team to possible
         # matchups for each week.
-        self.teams = dict((i, [copy.copy(team_matchups[i]) for _ in range(self.num_weeks)])
+        self.teams = dict((i, [self.randomize_matchups(team_matchups[i]) for _ in range(self.num_weeks)])
                           for i in teams)
+
+    def randomize_matchups(self, matchups):
+        matchups = copy.copy(matchups)
+        random.shuffle(matchups)
+        return matchups
 
     def generate_matchups(self, teams):
         return [(a, b) for a in teams for b in teams if a != b]
@@ -36,14 +70,35 @@ class Schedule:
     def remove_matchup(self, team, week, matchup):
         """Remove a possible matchup for the given team and week"""
         self.teams[team][week].remove(matchup)
+        # If the number of matchups for a week becomes 1, it means that matchup
+        # has been assigned. Invalidate any cached results for the team.
+        if len(self.teams[team][week]) == 1:
+            if not team in self.cache:
+                self.cache[team] = {}
+            self.cache[team][self.get_num_unassigned_weeks.__name__] = False
+            self.cache[team][self.get_num_potential_opponents.__name__] = False
+            self.cache[team][self.get_num_away_games.__name__] = False
+            self.cache[team][self.get_num_home_games.__name__] = False
 
+    @memoize
     def get_num_unassigned_weeks(self, team):
         """Get the number of unassigned weeks for the given team"""
         return sum(1 if len(x) > 1 else 0 for x in self.teams[team])
 
+    @memoize
     def get_num_potential_opponents(self, team):
         """Get the number of potential opponents remaining for the given team"""
         return len(self.get_potential_opponents(team))
+
+    @memoize
+    def get_num_home_games(self, team):
+        """Get the number of home games for the given team"""
+        return sum(1 if len(matchups) == 1 and matchups[0][0] == team else 0 for matchups in self.teams[team])
+
+    @memoize
+    def get_num_away_games(self, team):
+        """Get the number of away games for the given team"""
+        return self.num_weeks - self.get_num_unassigned_weeks(team) - self.get_num_home_games(team)
 
     def get_potential_opponents(self, team):
         """Get the list of potential oppoents remaining for the given team"""
@@ -55,13 +110,30 @@ class Schedule:
         opponents = opponents - set([team])
         return list(opponents)
 
+    def valid(self, team):
+        # Check that we can still fill the remaining weeks after this assignment.
+        remaining_opponent_count = self.get_num_potential_opponents(team)
+        remaining_weeks = self.get_num_unassigned_weeks(team)
+        if remaining_opponent_count < remaining_weeks:
+            return False
+        # Check that we don't have too many home or away games.
+        if self.get_num_home_games(team) > ((self.num_weeks + 1) & ~0x1) / 2:
+            return False
+        # Check that we don't have too many home or away games.
+        if self.get_num_away_games(team) > ((self.num_weeks + 1) & ~0x1) / 2:
+            return False
+        return True
+
     def test(self):
         """A set of unit tests"""
         # TODO: Ensure that if team a plays team b for week x in team a's
         # schedule, team b also thinks it is playing team a in week x.
         for team in self.teams:
             for week in range(self.num_weeks):
-                assert len(self.teams[team][week]) > 0
+                assert len(self.teams[team][week]) == 1
+                assert self.get_num_home_games(team) + self.get_num_away_games(team) == self.num_weeks
+                assert self.get_num_home_games(team) <= ((self.num_weeks + 1) & ~0x1) / 2
+                assert self.get_num_away_games(team) <= ((self.num_weeks + 1) & ~0x1) / 2
                 if len(self.teams[team][week]) == 1:
                     matchup = self.teams[team][week][0]
                     other_team = set(matchup) - set([team])
